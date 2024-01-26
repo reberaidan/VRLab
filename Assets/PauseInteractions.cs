b8258f90c9bf2bb65f67ffae8073c474f074b513
David Olivier <deotrenton@gmail.com>
2024-01-25 18:55:20 -0600

    Added an empty help box from the pause menu. Will fill as necessary

--- Assets/PauseInteractions.cs
+++ Assets/PauseInteractions.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
 using UnityEditor;
 using UnityEngine.SceneManagement;
 
@@ -8,10 +11,13 @@ public class PauseInteractions : MonoBehaviour
 {
 	
 	public GameObject PauseMenu;
-    // Start is called before the first frame update
-    void Start()
+	public GameObject HelpMenu;
+	
+	// Start is called before the first frame update
+    public void Start()
     {
         //PauseMenu.SetActive(false);
+		HelpMenu.SetActive(false);
     }
 
 	public void ResumeClicked(){
@@ -22,11 +28,23 @@ public class PauseInteractions : MonoBehaviour
 		
 	//}
 	
-	//public void HelpClicked(){
-		
-	//}
+	public void HelpClicked(){
+			HelpMenu.SetActive(true);
+			PauseMenu.SetActive(false);
+	}
 	
 	public void MainMenuClicked(){
 		SceneManager.LoadScene(0);
 	}
+	
+	public void ReturnClicked(){
+		HelpMenu.SetActive(false);
+		PauseMenu.SetActive(true);
+	}
+	
+	void Update()
+    {
+			
+	}
 }
+
