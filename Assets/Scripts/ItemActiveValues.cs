fa403ed49465bbc8e90be1d86b1bcf3d93f455d4
reberaidan <aidanweinreber@hotmail.com>
2024-02-01 15:22:36 -0600

    Object Values

---
+++ Assets/Scripts/ItemActiveValues.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemActiveValues : MonoBehaviour
+{
+
+    [SerializeField] float temperature;
+    [SerializeField] string contents;
+    [SerializeField] float boilingPoint;
+    [SerializeField] float roomTemp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+	private void OnTriggerStay(Collider other)
+	{
+		if (other.CompareTag("Heater"))
+		{
+            var heater = other.GetComponent<HeatingElement>();
+            //implement q = mcat
+            temperature = temperature + heater.heatingTemp;
+		}
+
+	}
+}
